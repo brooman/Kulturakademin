f824e789053d8bcfbc32db140d77670178e617c0
André Broman <bromanandre@gmail.com>
2019-09-02 16:49:32 +0200

    Added basic TextView

--- mock/server.js
+++ mock/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
@@ -6,20 +7,31 @@ const audio = require('./responses/audio')
 const text = require('./responses/text')
 const video = require('./responses/video')
 
+app.use(cors())
+
+app.get('/', (req, res) => {
+  console.log('/')
+  res.send({ audio, text, video })
+})
+
 app.get('/audio/', (req, res) => {
+  console.log('/audio/')
   res.send(audio)
 })
 
 app.get('/text/', (req, res) => {
+  console.log('/text/')
   res.send(text)
 })
 
 app.get('/video/', (req, res) => {
+  console.log('/video/')
   res.send(video)
 })
 
 app.get('/audio/:id', (req, res) => {
   const { id } = req.params
+  console.log('/audio/' + id)
 
   const resource = audio.find(function(obj) {
     return obj.id == id
@@ -30,6 +42,7 @@ app.get('/audio/:id', (req, res) => {
 
 app.get('/text/:id', (req, res) => {
   const { id } = req.params
+  console.log('/text/' + id)
 
   const resource = text.find(function(obj) {
     return obj.id == id
@@ -40,6 +53,7 @@ app.get('/text/:id', (req, res) => {
 
 app.get('/video/:id', (req, res) => {
   const { id } = req.params
+  console.log('/video/' + id)
 
   const resource = video.find(function(obj) {
     return obj.id == id
