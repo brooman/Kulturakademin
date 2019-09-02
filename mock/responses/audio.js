77ad62cdb19d624a925c8ed7e7c44969d7f8e5b1
André Broman <bromanandre@gmail.com>
2019-09-02 14:47:32 +0200

    Updated resources

--- mock/responses/audio.js
+++ mock/responses/audio.js
@@ -2,45 +2,50 @@ const Audio = [
   {
     id: 129,
     type: 'audio',
-    title:
-      '#129 Eeva Bolin - Ny organisation för kultur i grundskola och förskola',
+    title: '#129 Eeva Bolin - Ny organisation för kultur i grundskola och förskola',
     content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    url:
       'https://soundcloud.com/user-994747535/129-eeva-bolin-nya-organisation-for-kultur-i-grundskola-och-forskola',
     tags: ['podcast', 'learning'],
   },
   {
     id: 128,
     type: 'audio',
-    title:
-      '#128 Åsa Jansson – Regionala arrangörsstödet för barn & unga - kulturkatalogen väst',
+    title: '#128 Åsa Jansson – Regionala arrangörsstödet för barn & unga - kulturkatalogen väst',
     content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    url:
       'https://soundcloud.com/user-994747535/128-asa-jansson-regionala-arrangorsstodet-for-barn-unga-kulturkatalogen-vast',
     tags: ['podcast', 'learning'],
   },
   {
     id: 127,
     type: 'audio',
-    title:
-      '#127 Ida Kaivola - Kulturstöd och regler för utjämningsbidraget i Göteborg',
+    title: '#127 Ida Kaivola - Kulturstöd och regler för utjämningsbidraget i Göteborg',
     content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    url:
       'https://soundcloud.com/user-994747535/127-ida-kaivola-kulturstod-och-regler-for-utjamningsbidraget',
     tags: ['podcast', 'learning'],
   },
   {
     id: 126,
     type: 'audio',
-    title:
-      '#126 SFI & Film i Väst – filmfinansieringens möjligheter & utmaningar',
+    title: '#126 SFI & Film i Väst – filmfinansieringens möjligheter & utmaningar',
     content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    url:
       'https://soundcloud.com/user-994747535/126-sfi-film-i-vast-filmfinansieringens-mojligheter-utmaningar',
     tags: ['podcast', 'learning'],
   },
   {
     id: 125,
     type: 'audio',
-    title:
-      '#125 Åsa Söderberg – 12 års erfarenhet av publikutveckling på Skånes Dansteater',
+    title: '#125 Åsa Söderberg – 12 års erfarenhet av publikutveckling på Skånes Dansteater',
     content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    url:
       'https://soundcloud.com/user-994747535/125-asa-soderberg-12-ars-erfarenhet-av-publikutveckling-pa-skanes-dansteater',
     tags: ['podcast', 'learning'],
   },
@@ -50,9 +55,11 @@ const Audio = [
     title:
       '#124 Fading Lights - The Transition From Candle To Led with Chris Van Goethem (LIVE: 2019-05-06)',
     content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    url:
       'https://soundcloud.com/user-994747535/124-fading-lights-the-transition-from-candle-to-led-with-chris-van-goethem-live-2019-05-06',
     tags: ['podcast', 'learning'],
   },
-]
+];
 
-module.exports = Audio
+module.exports = Audio;
--- mock/responses/video.js
+++ mock/responses/video.js
@@ -3,45 +3,56 @@ const Video = [
     id: 129,
     type: 'video',
     title: 'Masterclass med Pia Olby - Vad är skönsång (del 3 av 4)',
-    content: 'https://www.youtube.com/watch?v=Nmf2V55mlgw',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    url: 'https://www.youtube.com/watch?v=Nmf2V55mlgw',
     tags: ['webcast', 'learning'],
   },
   {
     id: 128,
     type: 'video',
     title: 'Masterclass med Pia Olby - Vad är sånggestaltning (del 1 av 4)',
-    content: 'https://www.youtube.com/watch?v=OO-v7--wOtU',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    url: 'https://www.youtube.com/watch?v=OO-v7--wOtU',
     tags: ['webcast', 'learning'],
   },
   {
     id: 127,
     type: 'video',
-    title:
-      'Masterclass med Pia Olby - Textgestaltning och sånggestaltning (del 2 av 4)',
-    content: 'https://www.youtube.com/watch?v=qgjKQrgL0o0',
+    title: 'Masterclass med Pia Olby - Textgestaltning och sånggestaltning (del 2 av 4)',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    url: 'https://www.youtube.com/watch?v=qgjKQrgL0o0',
     tags: ['webcast', 'learning'],
   },
   {
     id: 126,
     type: 'video',
     title: 'Masterclass med Pia Olby - Kroppens betydelse i sång (del 4 av 4)',
-    content: 'https://www.youtube.com/watch?v=l2vcqaClw-A',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    url: 'https://www.youtube.com/watch?v=l2vcqaClw-A',
     tags: ['webcast', 'learning'],
   },
   {
     id: 125,
     type: 'video',
     title: 'NEW ON THE MAP: REInvent Studios',
-    content: 'https://www.youtube.com/watch?v=yGkn5KYk6sg',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    url: 'https://www.youtube.com/watch?v=yGkn5KYk6sg',
     tags: ['webcast', 'learning'],
   },
   {
     id: 124,
     type: 'video',
     title: 'https://www.youtube.com/watch?v=qvWzFtNPbB4',
-    content: 'https://www.youtube.com/watch?v=qvWzFtNPbB4',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    url: 'https://www.youtube.com/watch?v=qvWzFtNPbB4',
     tags: ['webcast', 'learning'],
   },
-]
+];
 
-module.exports = Video
+module.exports = Video;
