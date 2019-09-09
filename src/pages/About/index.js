d2eaa9fc68d023991ef5d5486ece3449606dc3cf
André Broman <bromanandre@gmail.com>
2019-09-09 15:05:52 +0200

    Eslint --fix

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const About = () => {
-  return <React.Fragment>About</React.Fragment>
+  return <>About</>
 }
 
 export default About
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,12 +4,12 @@ import Hero from '../../components/Hero'
 
 const Home = () => {
   return (
-    <React.Fragment>
+    <>
       <Hero
         image="jonathan-velasquez-c1ZN57GfDB0-unsplash.jpg"
         text="K-play det självklara valet"
       />
-    </React.Fragment>
+    </>
   )
 }
 
