ed723cd1bedb0c4bd0ecd56aaaf21264fdc07573
André Broman <bromanandre@gmail.com>
2019-09-09 15:04:59 +0200

    Hero image and header

---
+++ src/components/Hero/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styles from './index.module.scss'
+
+const Hero = props => {
+  const { image, text } = props
+
+  const style = {
+    backgroundImage: `url(images/${image})`,
+  }
+
+  return (
+    <div className={styles.hero} style={style}>
+      <h1 className={styles.heroText}>{text}</h1>
+    </div>
+  )
+}
+
+Hero.propTypes = {
+  image: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+}
+
+export default Hero
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
 const About = () => {
-  return (
-    <div>
-      About
-    </div>
-  )
+  return <React.Fragment>About</React.Fragment>
 }
 
 export default About
