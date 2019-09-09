5d5f80f1387936a9f92a4ab759ea3c63b2dd7628
André Broman <bromanandre@gmail.com>
2019-09-09 15:10:11 +0200

    Eslint being annoying :)

--- src/pages/ErrorPage/index.js
+++ src/pages/ErrorPage/index.js
@@ -7,7 +7,10 @@ const ErrorPage = props => {
   return (
     <div className={styles.errorPage}>
       <h1>An Error has occured</h1>
-      <h2>Status code: {statusCode}</h2>
+      <h2>
+        <span>Status code:</span>
+        <span>{statusCode}</span>
+      </h2>
     </div>
   )
 }
