7e4ca5181f9fcb0e4c82742fc9b8e9d7934d2ab9
André Broman <bromanandre@gmail.com>
2019-09-09 15:04:59 +0200

    Hero image and header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import Hero from '../../components/Hero'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <React.Fragment>
+      <Hero
+        image="jonathan-velasquez-c1ZN57GfDB0-unsplash.jpg"
+        text="K-play det självklara valet"
+      />
+    </React.Fragment>
+  )
 }
 
 export default Home
