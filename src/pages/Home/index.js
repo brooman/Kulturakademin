1cf106aadb42011294251a46557cd3716beb8688
André Broman <bromanandre@gmail.com>
2019-09-11 22:21:37 +0200

    Added basic categories with bad styling :)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Hero from '../../components/Hero'
+import Categories from '../../components/Categories'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         image="jonathan-velasquez-c1ZN57GfDB0-unsplash.jpg"
         text="K-play det självklara valet"
       />
+      <Categories />
     </>
   )
 }
