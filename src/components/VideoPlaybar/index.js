f8285ad2a39e95c594f7899ab72bd30bb437f9aa
Maria Eidland <maria.cederbom@gmail.com>
2019-09-12 18:58:28 +0200

    Added props

--- src/components/VideoPlaybar/index.js
+++ src/components/VideoPlaybar/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import styles from './index.module.scss'
 
 const VideoPlaybar = props => {
   return (
     <div className={styles.wrapper}>
-      <p>play</p>
-      <p>save</p>
+      <p>{props.play}</p>
+      <p>{props.save}</p>
     </div>
   )
+}
+
+VideoPlaybar.propTypes = {
+  play: PropTypes.string.isRequired,
+  save: PropTypes.string.isRequired,
 }
 
 export default VideoPlaybar
