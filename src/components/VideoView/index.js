b84cd2f554317f06ea3a018ee2bb7d5a0bed243b
root <root@DESKTOP-2TKMD5S.localdomain>
2019-09-12 16:30:00 +0200

    Use of playbar component and added video info elements

--- src/components/VideoView/index.js
+++ src/components/VideoView/index.js
@@ -1,35 +1,54 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
 import styles from './index.module.scss'
 
+import Hero from './../Hero'
+import VideoPlaybar from './../VideoPlaybar'
+
 const VideoView = props => {
-  const { title, content, url, tags } = props
+  const { year, episodeNo, episodeNos, title, content, url, tags } = props
   let tagCounter = 0
+
+  // <iframe title={title} width="560" height="315" src={url} />
 
   return (
     <div className="wrapper">
       <div>
-        <h2>{title}</h2>
-        <iframe title={title} width="560" height="315" src={url} />
-        <p>{content}</p>
-      </div>
-
-      <div className={styles.tags}>
-        {tags.map(tag => {
-          tagCounter += 1
-
-          return (
-            <p className={styles.tag} key={tagCounter}>
-              {tag}
+        <Hero image="video.png" text="" />
+        <VideoPlaybar play="play" save="save" />
+        <div className={styles.infoWrapper}>
+          <div className={styles.overviewInfo}>
+            <p>
+              {year} | Avsnitt {episodeNo}/{episodeNos}{' '}
             </p>
-          )
-        })}
+          </div>
+          <div className={styles.mainInfo}>
+            <h2 className={styles.title}>{title}</h2>
+            <p className={styles.content}>{content}</p>
+          </div>
+        </div>
+
+        <div className={styles.tags}>
+          {tags.map(tag => {
+            tagCounter += 1
+
+            return (
+              <p className={styles.tag} key={tagCounter}>
+                {tag}
+              </p>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
 }
 
 VideoView.propTypes = {
+  year: PropTypes.number.isRequired,
+  episodeNo: PropTypes.number.isRequired,
+  episodeNos: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
