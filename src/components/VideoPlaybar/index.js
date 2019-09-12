dbb2b5b38c3d0dd4e4913a10e82f5112f6ad2d52
root <root@DESKTOP-2TKMD5S.localdomain>
2019-09-12 18:50:22 +0200

    Removal of props

--- src/components/VideoPlaybar/index.js
+++ src/components/VideoPlaybar/index.js
@@ -1,20 +1,14 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import styles from './index.module.scss'
 
 const VideoPlaybar = props => {
   return (
     <div className={styles.wrapper}>
-      <p>{props.play}</p>
-      <p>{props.save}</p>
+      <p>play</p>
+      <p>save</p>
     </div>
   )
-}
-
-VideoPlaybar.propTypes = {
-  play: PropTypes.string.isRequired,
-  save: PropTypes.string.isRequired,
 }
 
 export default VideoPlaybar
