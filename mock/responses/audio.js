3b1ba73d0b3c1ad705d718520caf8c452fa30860
André Broman <bromanandre@gmail.com>
2019-09-13 20:42:26 +0200

    Added more data to mockserver

--- mock/responses/audio.js
+++ mock/responses/audio.js
@@ -2,6 +2,10 @@ const Audio = [
   {
     id: 129,
     type: 'audio',
+    year: '2017',
+    episodeNr: 1,
+    episodesInSeries: 1,
+    image: 'images/jakob-owens-CiUR8zISX60-unsplash.jpg',
     title: '#129 Eeva Bolin - Ny organisation för kultur i grundskola och förskola',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -11,6 +15,10 @@ const Audio = [
   {
     id: 128,
     type: 'audio',
+    year: '2017',
+    episodeNr: 1,
+    episodesInSeries: 1,
+    image: 'images/jakob-owens-CiUR8zISX60-unsplash.jpg',
     title: '#128 Åsa Jansson – Regionala arrangörsstödet för barn & unga - kulturkatalogen väst',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -20,6 +28,10 @@ const Audio = [
   {
     id: 127,
     type: 'audio',
+    year: '2017',
+    episodeNr: 1,
+    episodesInSeries: 1,
+    image: 'images/jakob-owens-CiUR8zISX60-unsplash.jpg',
     title: '#127 Ida Kaivola - Kulturstöd och regler för utjämningsbidraget i Göteborg',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -29,6 +41,10 @@ const Audio = [
   {
     id: 126,
     type: 'audio',
+    year: '2017',
+    episodeNr: 1,
+    episodesInSeries: 1,
+    image: 'images/jakob-owens-CiUR8zISX60-unsplash.jpg',
     title: '#126 SFI & Film i Väst – filmfinansieringens möjligheter & utmaningar',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -38,6 +54,10 @@ const Audio = [
   {
     id: 125,
     type: 'audio',
+    year: '2017',
+    episodeNr: 1,
+    episodesInSeries: 1,
+    image: 'images/jakob-owens-CiUR8zISX60-unsplash.jpg',
     title: '#125 Åsa Söderberg – 12 års erfarenhet av publikutveckling på Skånes Dansteater',
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -47,6 +67,10 @@ const Audio = [
   {
     id: 124,
     type: 'audio',
+    year: '2017',
+    episodeNr: 1,
+    episodesInSeries: 1,
+    image: 'images/jakob-owens-CiUR8zISX60-unsplash.jpg',
     title:
       '#124 Fading Lights - The Transition From Candle To Led with Chris Van Goethem (LIVE: 2019-05-06)',
     content:
