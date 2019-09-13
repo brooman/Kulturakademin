a30ebba5b8e20c1dc3bf911e9fa9f31df7c8f1f0
Ida Hallner <bee_ida@hotmail.com>
2019-09-13 12:45:37 +0200

    Changed width

--- src/components/DiscoverCard/index.js
+++ src/components/DiscoverCard/index.js
@@ -8,22 +8,23 @@ import styles from './index.module.scss'
 const DiscoverCard = props => {
   const { order, item, displayMode } = props
   const { id, type, title, image, content } = item
+  const year = '2019'
   const img = { backgroundImage: `url('images/${image}')` }
-
   const viewStyle = displayMode === 'list' ? styles.listView : styles.gridView
   const background = order % 2 === 1 ? styles.dark : styles.light
   const link = `/view/${type}/${id}`
-
+  console.log(year)
   return (
     <div className={[styles.card, viewStyle, background].join(' ')}>
       <div className={styles.image} style={img} />
       <div className={styles.body}>
         <div className={styles.content}>
           <h3>{title}</h3>
+          <p>{year}</p>
           <p className={styles.description}>{content}</p>
         </div>
         <div className={styles.readMore}>
-          <p>Read more</p>
+          <p>Läs mer</p>
           <div className={styles.arrow}>
             <ReadMoreIcon />
           </div>
