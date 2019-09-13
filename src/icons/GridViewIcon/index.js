84b6d22043df88b44c72b8c48208c599021ca4d6
Ida Hallner <bee_ida@hotmail.com>
2019-09-13 17:09:45 +0200

    Added a icon for grid view

---
+++ src/icons/GridViewIcon/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable max-len */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const GridViewiconIcon = props => {
+  const { color } = props
+
+  return (
+    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="0.611328" width="13.0612" height="13.0612" fill="#C4C4C4" />
+      <rect x="16.9379" width="13.0612" height="13.0612" fill="#C4C4C4" />
+      <rect x="16.9379" y="16.3265" width="13.0612" height="13.0612" fill="#C4C4C4" />
+      <rect x="0.611328" y="16.3265" width="13.0612" height="13.0612" fill="#C4C4C4" />
+    </svg>
+  )
+}
+
+GridViewiconIcon.propTypes = {
+  color: PropTypes.string,
+}
+
+GridViewiconIcon.defaultProps = {
+  color: '#fff',
+}
+
+export default GridViewiconIcon
