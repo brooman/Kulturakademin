e1e9baaa2cb3aacabd2977d9eb58d1b5e87670db
Ida Hallner <bee_ida@hotmail.com>
2019-09-17 13:51:33 +0200

    Updated data with highlighted

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,4 +1,25 @@
 export default {
+  highlighted: [
+    {
+      id: 129,
+      type: 'audio',
+      title: '#129 Eeva Bolin - Ny organisation för kultur i grundskola och förskola',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      trackingId: '642376728',
+      tags: ['podcast', 'learning'],
+    },
+    {
+      id: 128,
+      type: 'audio',
+      title: '#128 Åsa Jansson – Regionala arrangörsstödet för barn & unga - kulturkatalogen väst',
+      content:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      trackingId: '642375006',
+      tags: ['podcast', 'learning'],
+    },
+  ],
+
   audio: [
     {
       id: 129,
