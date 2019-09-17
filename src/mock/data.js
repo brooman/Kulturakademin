915e42bf299168a736985e4adfd6460eb5196a9f
Ida Hallner <bee_ida@hotmail.com>
2019-09-17 19:22:10 +0200

    Added data for year and length

--- src/mock/data.js
+++ src/mock/data.js
@@ -3,7 +3,11 @@ export default {
     {
       id: 129,
       type: 'video',
-      title: 'Masterclass med Pia Olby - Vad är skönsång (del 3 av 4)',
+      title: 'Masterclass med Pia Olby - Vad är skönsång',
+      episodeNr: '3',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       url: 'https://www.youtube.com/embed/Nmf2V55mlgw',
@@ -14,6 +18,10 @@ export default {
       type: 'audio',
       title: 'Eeva Bolin - Ny organisation för kultur i grundskola och förskola',
       image: 'jeremy-yap-J39X2xX_8CQ-unsplash.jpg',
+      episodeNr: '2',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       trackingId: '642376728',
@@ -26,6 +34,10 @@ export default {
       id: 129,
       type: 'audio',
       title: 'Eeva Bolin - Ny organisation för kultur i grundskola och förskola',
+      episodeNr: '2',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       trackingId: '642376728',
@@ -35,6 +47,10 @@ export default {
       id: 128,
       type: 'audio',
       title: 'Åsa Jansson – Regionala arrangörsstödet för barn & unga - kulturkatalogen väst',
+      episodeNr: '2',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       trackingId: '642375006',
@@ -44,6 +60,10 @@ export default {
       id: 127,
       type: 'audio',
       title: 'Ida Kaivola - Kulturstöd och regler för utjämningsbidraget i Göteborg',
+      episodeNr: '2',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       trackingId: '642369729',
@@ -53,6 +73,10 @@ export default {
       id: 126,
       type: 'audio',
       title: 'SFI & Film i Väst – filmfinansieringens möjligheter & utmaningar',
+      episodeNr: '2',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       trackingId: '641768196',
@@ -62,6 +86,10 @@ export default {
       id: 125,
       type: 'audio',
       title: 'Åsa Söderberg – 12 års erfarenhet av publikutveckling på Skånes Dansteater',
+      episodeNr: '2',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       trackingId: '639535935',
@@ -71,6 +99,10 @@ export default {
       id: 124,
       type: 'audio',
       title: 'Fading Lights - The Transition From Candle To Led with Chris Van Goethem',
+      episodeNr: '2',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       trackingId: '639016008',
@@ -81,7 +113,11 @@ export default {
     {
       id: 129,
       type: 'video',
-      title: 'Masterclass med Pia Olby - Vad är skönsång (del 3 av 4)',
+      title: 'Masterclass med Pia Olby - Vad är skönsång',
+      episodeNr: '3',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       url: 'https://www.youtube.com/embed/Nmf2V55mlgw',
@@ -90,7 +126,11 @@ export default {
     {
       id: 128,
       type: 'video',
-      title: 'Masterclass med Pia Olby - Vad är sånggestaltning (del 1 av 4)',
+      title: 'Masterclass med Pia Olby - Vad är sånggestaltning',
+      episodeNr: '1',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       url: 'https://www.youtube.com/embed/OO-v7--wOtU',
@@ -99,7 +139,11 @@ export default {
     {
       id: 127,
       type: 'video',
-      title: 'Masterclass med Pia Olby - Textgestaltning och sånggestaltning (del 2 av 4)',
+      title: 'Masterclass med Pia Olby - Textgestaltning och sånggestaltning',
+      episodeNr: '2',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       url: 'https://www.youtube.com/embed/qgjKQrgL0o0',
@@ -108,7 +152,11 @@ export default {
     {
       id: 126,
       type: 'video',
-      title: 'Masterclass med Pia Olby - Kroppens betydelse i sång (del 4 av 4)',
+      title: 'Masterclass med Pia Olby - Kroppens betydelse i sång',
+      episodeNr: '4',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       url: 'https://www.youtube.com/embed/l2vcqaClw-A',
@@ -118,6 +166,10 @@ export default {
       id: 125,
       type: 'video',
       title: 'NEW ON THE MAP: REInvent Studios',
+      episodeNr: '2',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       url: 'https://www.youtube.com/embed/yGkn5KYk6sg',
@@ -127,6 +179,10 @@ export default {
       id: 124,
       type: 'video',
       title: 'https://www.youtube.com/watch?v=qvWzFtNPbB4',
+      episodeNr: '2',
+      episodesInSeries: '4',
+      year: '2019',
+      length: '34',
       content:
         'Lorem ipsum dolor sit amet, con sectetur adipiscing elit. Nibh diam nulla amet, nunc tempus ante. ',
       url: 'https://www.youtube.com/watch?v=qvWzFtNPbB4',
