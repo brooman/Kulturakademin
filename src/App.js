75c060088cd2c5a2d8eaa814270e07ba890a97a2
Maria Eidland <maria.cederbom@gmail.com>
2019-09-18 16:42:10 +0200

    Removed unused state

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ class App extends Component {
 
     this.state = {
       menuShown: false,
-      pwaPopUpShown: true,
     }
   }
 
@@ -35,7 +34,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header>
-            <Link to={process.env.PUBLIC_URL + '/'}>
+            <Link to={`${process.env.PUBLIC_URL}/`}>
               <LogoIcon />
             </Link>
 
@@ -48,21 +47,21 @@ class App extends Component {
           </header>
           <Menu show={menuShown} />
           <div className="content">
-            <Route path={process.env.PUBLIC_URL + '/'} exact component={Home} />
-            <Route path={process.env.PUBLIC_URL + '/discover'} component={Discover} />
-            <Route path={process.env.PUBLIC_URL + '/view/:type/:id'} component={ContentView} />
-            <Route path={process.env.PUBLIC_URL + '/about'} component={About} />
+            <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home} />
+            <Route path={`${process.env.PUBLIC_URL}/discover`} component={Discover} />
+            <Route path={`${process.env.PUBLIC_URL}/view/:type/:id`} component={ContentView} />
+            <Route path={`${process.env.PUBLIC_URL}/about`} component={About} />
           </div>
           <footer>
             <ul>
               <li>
-                <Link to={process.env.PUBLIC_URL + '/'}>Home</Link>
+                <Link to={`${process.env.PUBLIC_URL}/`}>Home</Link>
               </li>
               <li>
-                <Link to={process.env.PUBLIC_URL + '/view/video/128'}>View</Link>
+                <Link to={`${process.env.PUBLIC_URL}/view/video/128`}>View</Link>
               </li>
               <li>
-                <Link to={process.env.PUBLIC_URL + '/about/'}>About</Link>
+                <Link to={`${process.env.PUBLIC_URL}/about/`}>About</Link>
               </li>
             </ul>
           </footer>
