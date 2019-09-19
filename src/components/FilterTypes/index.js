e638c9c88e60ddf21209cddf02a40a5a85fc24e9
root <root@DESKTOP-2TKMD5S.localdomain>
2019-09-19 11:09:30 +0200

    Functionality for highligting active button

--- src/components/FilterTypes/index.js
+++ src/components/FilterTypes/index.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import styles from './index.module.scss'
 
-const FilterTypes = () => {
-  return (
-    <div className={styles.row}>
-      <p>Filtrera</p>
-      <div>
-        <button type="button" className={styles.active}>
-          Allt
-        </button>
-        <button type="button">Video</button>
-        <button type="button">Pod</button>
-      </div>
-    </div>
-  )
-}
+const buttons = ['Allt', 'Video', 'Pod']
 
+class FilterTypes extends Component {
+  state = {
+    active: 'Allt',
+  }
+  _handleClick(button) {
+    this.setState({ active: button })
+  }
+
+  render() {
+    const activeStyle = { color: '#ff3333' }
+    const notActiveStyle = { color: '#fff' }
+    return (
+      <div className={styles.row}>
+        {buttons.map(button => (
+          <button
+            style={this.state.active === button ? activeStyle : notActiveStyle}
+            onClick={this._handleClick.bind(this, button)}
+          >
+            {button}
+          </button>
+        ))}
+      </div>
+    )
+  }
+}
 export default FilterTypes
