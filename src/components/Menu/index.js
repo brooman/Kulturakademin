9b8071dbaea33a13f87c3736de08835066f6a1e8
Ida Hallner <bee_ida@hotmail.com>
2019-09-20 15:45:06 +0200

    Remove toggle and array

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,109 +1,32 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import HamburgerIcon from '../../icons/HamburgerIcon'
-
+import React from 'react'
 import PropTypes from 'prop-types'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 import styles from './index.module.scss'
 
-class Menu extends Component {
-  constructor() {
-    super()
+const Menu = props => {
+  const { show } = props
 
-    this.state = {
-      showMenu: false,
-      categories: [
-        {
-          id: 0,
-          name: 'Ljud',
-          link: '/discover/audio',
-        },
-        {
-          id: 1,
-          name: 'Ljus',
-          link: '/discover/light',
-        },
-        {
-          id: 2,
-          name: 'Dans',
-          link: '/discover/dance',
-        },
-        {
-          id: 3,
-          name: 'Musik',
-          link: '/discover/music',
-        },
-        {
-          id: 4,
-          name: 'TV',
-          link: '/discover/television',
-        },
-        {
-          id: 5,
-          name: 'Tal',
-          link: '/discover/television',
-        },
-      ],
-    }
-    this.showMenu = this.showMenu.bind(this)
-    this.closeMenu = this.closeMenu.bind(this)
-  }
-
-  showMenu(event) {
-    event.preventDefault()
-
-    this.setState({ showMenu: true }, () => {
-      document.addEventListener('click', this.closeMenu)
-    })
-  }
-
-  closeMenu() {
-    this.setState({ showMenu: false }, () => {
-      document.removeEventListener('click', this.closeMenu)
-    })
-  }
-
-  render() {
-    const { categories } = this.state
+  if (show) {
     return (
-      <div>
-        <div className={styles.menu}>
-          <button onClick={this.showMenu}>
-            <HamburgerIcon />
-          </button>
-          {this.state.showMenu ? (
-            <div>
-              {categories.map(category => {
-                const { id, name, link } = category
-                return (
-                  <div key={id} className={styles.category}>
-                    <Link to={process.env.PUBLIC_URL + link}>{name}</Link>
-                  </div>
-                )
-              })}
-              <Link to="/">
-                <p>Fortsätt>/Sparade</p>
-              </Link>
-              <Link to="/">
-                <p>Senaste</p>
-              </Link>
-              <Link to="/">
-                <p>Mest visade</p>
-              </Link>
-              <Link to="/">
-                <p>Taggar</p>
-              </Link>
-              <Link to="/">
-                <p>Om</p>
-              </Link>
-              <Link to="/">
-                <p>Mest visade</p>
-              </Link>
-            </div>
-          ) : null}
-        </div>
+      <div className={styles.menu}>
+        <Link to={`${process.env.PUBLIC_URL}/discover/audio`}>Ljud</Link>
+        <Link to={`${process.env.PUBLIC_URL}/discover/light`}>Ljus</Link>
+        <Link to={`${process.env.PUBLIC_URL}/discover/dance`}>Dans</Link>
+        <Link to={`${process.env.PUBLIC_URL}/discover/music`}>Musik</Link>
+        <Link to={`${process.env.PUBLIC_URL}/discover/television`}>Tv</Link>
+        <Link to={`${process.env.PUBLIC_URL}/discover/dance`}>Tal</Link>
+        <Link to={`${process.env.PUBLIC_URL}/discover/dance`}>Fortsätt/Sparade</Link>
+        <Link to={`${process.env.PUBLIC_URL}/discover/dance`}>Senaste</Link>
+        <Link to={`${process.env.PUBLIC_URL}/discover/dance`}>Mest visade</Link>
+        <Link to={`${process.env.PUBLIC_URL}/discover/dance`}>Taggar</Link>
+        <Link to={`${process.env.PUBLIC_URL}/discover/dance`}>Om</Link>
+        <Link to={`${process.env.PUBLIC_URL}/discover/dance`}>Inställningar</Link>
       </div>
     )
   }
+
+  return null
 }
 
 Menu.propTypes = {
