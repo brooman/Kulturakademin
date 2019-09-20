557b86c66fc010124943e5fccff292a7df3d0c0a
Ida Hallner <bee_ida@hotmail.com>
2019-09-20 10:48:15 +0200

    Added state to close menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,61 +1,91 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import HamburgerIcon from '../../icons/HamburgerIcon'
 
 import PropTypes from 'prop-types'
 import styles from './index.module.scss'
 
-const categories = [
-  {
-    id: 0,
-    name: 'Ljud',
-    link: '/discover/audio',
-  },
-  {
-    id: 1,
-    name: 'Ljus',
-    link: '/discover/light',
-  },
-  {
-    id: 2,
-    name: 'Dans',
-    link: '/discover/dance',
-  },
-  {
-    id: 3,
-    name: 'Musik',
-    link: '/discover/music',
-  },
-  {
-    id: 4,
-    name: 'TV',
-    link: '/discover/television',
-  },
-  {
-    id: 5,
-    name: 'Tal',
-    link: '/discover/television',
-  },
-]
+class Menu extends Component {
+  constructor() {
+    super()
 
-const Menu = props => {
-  const { show } = props
+    this.state = {
+      showMenu: false,
+      categories: [
+        {
+          id: 0,
+          name: 'Ljud',
+          link: '/discover/audio',
+        },
+        {
+          id: 1,
+          name: 'Ljus',
+          link: '/discover/light',
+        },
+        {
+          id: 2,
+          name: 'Dans',
+          link: '/discover/dance',
+        },
+        {
+          id: 3,
+          name: 'Musik',
+          link: '/discover/music',
+        },
+        {
+          id: 4,
+          name: 'TV',
+          link: '/discover/television',
+        },
+        {
+          id: 5,
+          name: 'Tal',
+          link: '/discover/television',
+        },
+      ],
+    }
+    this.showMenu = this.showMenu.bind(this)
+    this.closeMenu = this.closeMenu.bind(this)
+  }
 
-  if (show) {
+  showMenu(event) {
+    event.preventDefault()
+
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu)
+    })
+  }
+
+  closeMenu() {
+    this.setState({ showMenu: false }, () => {
+      document.removeEventListener('click', this.closeMenu)
+    })
+  }
+
+  render() {
+    const { categories } = this.state
     return (
-      <div className={styles.menu}>
-        {categories.map(category => {
-          const { id, name, link } = category
-          return (
-            <div key={id} className={styles.category}>
-              <Link to={process.env.PUBLIC_URL + link}>{name}</Link>
+      <div>
+        <div className={styles.menu}>
+          <button onClick={this.showMenu}>
+            <HamburgerIcon />
+          </button>
+          {this.state.showMenu ? (
+            <div>
+              {categories.map(category => {
+                const { id, name, link } = category
+                return (
+                  <div key={id} className={styles.category}>
+                    <Link to={process.env.PUBLIC_URL + link}>{name}</Link>
+                  </div>
+                )
+              })}
             </div>
-          )
-        })}
+          ) : null}
+        </div>
       </div>
     )
   }
-
-  return null
 }
 
 Menu.propTypes = {
