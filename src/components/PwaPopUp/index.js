30a632fb5463dd99c1c11cdde1c07a26d83330c6
Maria Eidland <maria.cederbom@gmail.com>
2019-09-22 10:49:10 +0200

    Removed out commented code

--- src/components/PwaPopUp/index.js
+++ src/components/PwaPopUp/index.js
@@ -46,37 +46,6 @@ const PwaPopUp = () => {
         </button>
       </div>
     </div>
-    // <div
-    //   className={[
-    //     clicked ? styles.slideDown : styles.show,
-    //     showPwaPopUp ? styles.hide : styles.show,
-    //     styles.wrapper,
-    //   ].join(' ')}
-    // >
-    //   <div className={styles.bar}>
-    //     <p>Spara som Desktop App</p>
-    //     <div className={styles.buttonWrapper}>
-    //       <button
-    //         type="button"
-    //         onClick={() => {
-    //           setCookieYes()
-    //           setClicked(true)
-    //         }}
-    //       >
-    //         Ja
-    //       </button>
-    //       <button
-    //         type="button"
-    //         onClick={() => {
-    //           setCookieLater()
-    //           setClicked(true)
-    //         }}
-    //       >
-    //         Senare
-    //       </button>
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
 
