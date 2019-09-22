4cd00107226f1d0dc205f6ce13ccd0560337e9d1
root <root@DESKTOP-2TKMD5S.localdomain>
2019-09-22 11:08:17 +0200

    Added CookiesPopUp

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import LogoIcon from './icons/LogoIcon'
 import HamburgerIcon from './icons/HamburgerIcon'
 import SearchIcon from './icons/SearchIcon'
 import PwaPopUp from './components/PwaPopUp'
+import CookiesPopUp from './components/CookiesPopUp'
 import Player from './components/Player'
 
 import { PlayerContextProvider } from './Hooks/PlayerContextProvider'
@@ -38,7 +39,7 @@ class App extends Component {
         <Router>
           <div className="App">
             <header>
-              <Link to={`${process.env.PUBLIC_URL  }/`}>
+              <Link to={`${process.env.PUBLIC_URL}/`}>
                 <LogoIcon />
               </Link>
 
@@ -52,27 +53,28 @@ class App extends Component {
             <Menu show={menuShown} />
 
             <div className="content">
-              <Route path={`${process.env.PUBLIC_URL  }/`} exact component={Home} />
-              <Route path={`${process.env.PUBLIC_URL  }/discover`} component={Discover} />
-              <Route path={`${process.env.PUBLIC_URL  }/view/:type/:id`} component={ContentView} />
-              <Route path={`${process.env.PUBLIC_URL  }/about`} component={About} />
+              <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home} />
+              <Route path={`${process.env.PUBLIC_URL}/discover`} component={Discover} />
+              <Route path={`${process.env.PUBLIC_URL}/view/:type/:id`} component={ContentView} />
+              <Route path={`${process.env.PUBLIC_URL}/about`} component={About} />
             </div>
 
             <footer>
               <ul>
                 <li>
-                  <Link to={`${process.env.PUBLIC_URL  }/`}>Home</Link>
+                  <Link to={`${process.env.PUBLIC_URL}/`}>Home</Link>
                 </li>
                 <li>
-                  <Link to={`${process.env.PUBLIC_URL  }/view/video/128`}>View</Link>
+                  <Link to={`${process.env.PUBLIC_URL}/view/video/128`}>View</Link>
                 </li>
                 <li>
-                  <Link to={`${process.env.PUBLIC_URL  }/about/`}>About</Link>
+                  <Link to={`${process.env.PUBLIC_URL}/about/`}>About</Link>
                 </li>
               </ul>
             </footer>
-            
+
             <PwaPopUp />
+            <CookiesPopUp />
             <Player />
           </div>
         </Router>
