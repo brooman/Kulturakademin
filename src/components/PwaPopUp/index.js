1813843a4c63eee9e456f9dde7c0dbed498e4188
root <root@DESKTOP-2TKMD5S.localdomain>
2019-09-22 10:40:32 +0200

    Removed nested ternary

--- src/components/PwaPopUp/index.js
+++ src/components/PwaPopUp/index.js
@@ -19,34 +19,64 @@ const PwaPopUp = () => {
   return (
     <div
       className={[
-        showPwaPopUp ? styles.hide : styles.show && clicked ? styles.slideDown : styles.show,
-        styles.wrapper,
+        styles.bar,
+        clicked ? styles.slideDown : styles.slideUp,
+        showPwaPopUp ? styles.hide : styles.show,
       ].join(' ')}
     >
-      <div className={styles.bar}>
-        <p>Spara som Desktop App</p>
-        <div className={styles.buttonWrapper}>
-          <button
-            type="button"
-            onClick={() => {
-              setCookieYes()
-              setClicked(true)
-            }}
-          >
-            Ja
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              setCookieLater()
-              setClicked(true)
-            }}
-          >
-            Senare
-          </button>
-        </div>
+      <p>Spara som desktop app</p>
+      <div className={styles.buttonWrapper}>
+        <button
+          type="button"
+          onClick={() => {
+            setCookieYes()
+            setClicked(true)
+          }}
+        >
+          Ja
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            setCookieLater()
+            setClicked(true)
+          }}
+        >
+          Senare
+        </button>
       </div>
     </div>
+    // <div
+    //   className={[
+    //     clicked ? styles.slideDown : styles.show,
+    //     showPwaPopUp ? styles.hide : styles.show,
+    //     styles.wrapper,
+    //   ].join(' ')}
+    // >
+    //   <div className={styles.bar}>
+    //     <p>Spara som Desktop App</p>
+    //     <div className={styles.buttonWrapper}>
+    //       <button
+    //         type="button"
+    //         onClick={() => {
+    //           setCookieYes()
+    //           setClicked(true)
+    //         }}
+    //       >
+    //         Ja
+    //       </button>
+    //       <button
+    //         type="button"
+    //         onClick={() => {
+    //           setCookieLater()
+    //           setClicked(true)
+    //         }}
+    //       >
+    //         Senare
+    //       </button>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
