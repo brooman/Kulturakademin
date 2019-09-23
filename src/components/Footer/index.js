742ea83fdde44d934e178280aed50d84921fcc16
Ida Hallner <bee_ida@hotmail.com>
2019-09-23 10:27:13 +0200

    Created component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import MobileIcon from '../../icons/MobileIcon'
+
+import styles from './index.module.scss'
+
+const Footer = props => {
+  const { text } = props
+
+  return (
+    <div className={styles.footer}>
+      <MobileIcon />
+      <h1 className={styles.footerText}>{text}</h1>
+    </div>
+  )
+}
+
+Footer.propTypes = {
+  image: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+}
+
+export default Footer
