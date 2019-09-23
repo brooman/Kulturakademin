99ebf46197cba7b70782de5c846733156ad4455b
root <root@DESKTOP-2TKMD5S.localdomain>
2019-09-23 15:37:40 +0200

    Changed name on type in url to categoryType

--- src/App.js
+++ src/App.js
@@ -56,7 +56,10 @@ class App extends Component {
             <div className="content">
               <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home} />
               <Route path={`${process.env.PUBLIC_URL}/discover`} component={Discover} />
-              <Route path={`${process.env.PUBLIC_URL}/category`} component={CategoryView} />
+              <Route
+                path={`${process.env.PUBLIC_URL}/category/:categoryType`}
+                component={CategoryView}
+              />
               <Route path={`${process.env.PUBLIC_URL}/view/:type/:id`} component={ContentView} />
               <Route path={`${process.env.PUBLIC_URL}/about`} component={About} />
             </div>
