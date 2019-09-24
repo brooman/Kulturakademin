30970c1f2477c838767193560a7220b73bff0c47
Ida Hallner <bee_ida@hotmail.com>
2019-09-24 10:12:34 +0200

    Added more text to hero

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,9 @@ const Home = () => {
     <>
       <Hero
         image="images/jonathan-velasquez-c1ZN57GfDB0-unsplash.jpg"
-        text="K-play det självklara valet"
+        title="K-Play"
+        text="– Det självklara valet för vetgiriga"
+        intro="Här kan du lyssna och se på utbildande poddar och videoklipp. Nunc consectetur vel sed turpis sed aliquet lacus, suspendisse. Id dui magna tincidunt ut."
       />
       <Categories />
     </>
