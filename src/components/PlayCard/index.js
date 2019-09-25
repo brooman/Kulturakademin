a0593b41d34791eadf7629c6fc9d44bcf0173b71
Ida Hallner <bee_ida@hotmail.com>
2019-09-25 10:44:44 +0200

    Added progressbar and playicon

--- src/components/PlayCard/index.js
+++ src/components/PlayCard/index.js
@@ -2,8 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import ReadMoreIcon from '../../icons/ReadMoreIcon'
-import PodIcon from '../../icons/PodIcon'
-import VideoIcon from '../../icons/VideoIcon'
+import PlayIcon from '../../icons/PlayIcon'
 
 import styles from './index.module.scss'
 
@@ -27,21 +26,52 @@ const PlayCard = props => {
     }
   }
 
+  const ProgressBar = props => {
+    const { currentTime, duration } = props
+
+    const progress = (currentTime / duration) * 100
+
+    const progressBar = {
+      backgroundImage: `linear-gradient(to right, #cd7b2a, #cd7b2a ${progress}%, #fff ${progress}%, #fff 100%)`,
+    }
+
+    return <div className={styles.progressBar} style={progressBar} />
+  }
+
   const viewStyle = getViewStyle(displayMode)
 
   const link = `/view/${item.type}/${item.id}`
 
+  // return (
+  //   <div className={[styles.card, viewStyle].join(' ')}>
+  //     <div className={styles.imageWrapper}>
+  //       <div className={styles.image} style={img} />
+  //     </div>
+  //     <div className={styles.body}>
+  //       <ProgressBar />
+  //       <Link to={link}>
+  //         <div className={styles.readMore}>
+  //           <p>{content}</p>
+  //           <div className={styles.arrow}>
+  //             <PlayIcon />
+  //           </div>
+  //         </div>
+  //       </Link>
+  //     </div>
+  //   </div>
+  // )
   return (
     <div className={[styles.card, viewStyle].join(' ')}>
       <div className={styles.imageWrapper}>
         <div className={styles.image} style={img} />
       </div>
       <div className={styles.body}>
+        <ProgressBar />
         <Link to={link}>
-          <div className={styles.readMore}>
-            <p>{content}</p>
-            <div className={styles.arrow}>
-              <ReadMoreIcon />
+          <div className={styles.content}>
+            <p className={styles.description}>{content}</p>
+            <div className={styles.playIcon}>
+              <PlayIcon />
             </div>
           </div>
         </Link>
