03f9297e2628c211ba459f87a038b721b2cfae18
Maria Eidland <maria.cederbom@gmail.com>
2019-09-25 10:54:35 +0200

    To update to master

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -43,7 +43,7 @@ const internalPlayer = (resource, playing, setRef, minimized) => {
           />
         </div>
       )
-    case 'audio':
+    case 'pod':
       return (
         <>
           <PreviewImage className={styles.preview} image={resource.image} />
