09da845e00a85c09c2a1b5dd59ea455bf18daa62
brooman <bromanandre@gmail.com>
2019-10-04 14:14:27 +0000

    Deploying to gh-pages from master a73a2bf7029339222216a9fc63552a20370b0530

---
+++ precache-manifest.0372ec55c97d1b2f645e3d04e847eae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb67c9d0514b1211966e3ea2920d36e8",
+    "url": "/Kulturakademin/index.html"
+  },
+  {
+    "revision": "424a6af347417d53565b",
+    "url": "/Kulturakademin/static/css/main.dcc22818.chunk.css"
+  },
+  {
+    "revision": "82337fcd0d2895278429",
+    "url": "/Kulturakademin/static/js/2.0c08ff2c.chunk.js"
+  },
+  {
+    "revision": "424a6af347417d53565b",
+    "url": "/Kulturakademin/static/js/main.d824feeb.chunk.js"
+  },
+  {
+    "revision": "3005c69bfb451194c57f",
+    "url": "/Kulturakademin/static/js/runtime~main.30640ec4.js"
+  }
+]);
